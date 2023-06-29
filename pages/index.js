533431cc49fcd6db22355e7603ad4b29556aea14
Alan Varela Ferreira <49076154+alanvf1@users.noreply.github.com>
2023-06-29 13:38:41 +0000

    teste vercel

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Confio em você!! Seja o mundo que deseja mudar. ❤</h1>
+    return <h1>Confio em você!! Seja o mundo que deseja mudar. ❤ Teste</h1>
 }
 
 export default Home;
